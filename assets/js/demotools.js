e9f9ca9bba9d0d04d92a18ea9456cf03abd10231
Frank Taylor <paceaux@madebypaceaux.com>
2015-01-06 13:30:56 -0600

    added function for removing classes. also context menu

--- assets/js/demotools.js
+++ assets/js/demotools.js
@@ -7,8 +7,8 @@ HTMLElement.prototype.removeClassByPartialName = function (partialName) {
         if (elClassName.indexOf(partialName) !== -1) {
             el.className = el.className.replace(elClassName,'');
         }
-        el.className = el.className.replace(/\s+/g," ");
     });
+    el.className = el.className.replace(/\s+/g," ");
 };
 
 /*
@@ -24,20 +24,9 @@ demoTools = {
     },
     data: {},
     helpers: {
-        removeClassByPartial: function (element, classPartial) {
-            var classArray = element.className.split(' '),
-                elToRemove;
-            classArray.forEach(function(classItem, i) {
-                if (classItem.indexOf(classPartial) !== -1) {
-                    console.log(classItem);
-                    element.className = element.className.replace(classItem,'');
-                    element.className = element.className.replace(/\s+/g," ");
-                }
-            });
-        }
     },
     uiEvents: {
-        justifyChange: function (e) {
+        inputChange: function (e) {
             var _this = demoTools,
                 value = this.value,
                 classPrefix = this.dataset.classprefix,
@@ -64,16 +53,36 @@ demoTools = {
                 targetEl = document.querySelector(this.dataset.cloneofselector),
                 clone =    targetEl.cloneNode(true);
             targetEl.parentNode.appendChild(clone);
+            _this.activatePlugins();
 
+        },
+        figureContextMenu: function (e) {
+            e.preventDefault();
+            console.log(e);
+            var coords = {x: e.clientX, y: e.clientY},
+                menu = document.getElementById('contextmenu');
+            $('.promo').find('.hasMenu').removeClass('hasMenu');
+            $(this).addClass('hasMenu');
+            menu.style.display = "block";
+            menu.style.position = "fixed";
+            menu.style.left = coords.x + 'px';
+            menu.style.top = coords.y + 'px';
+        },
+        figureContextMenuMouseout: function (e) {
+            var menu = document.getElementById('contextmenu');
+            menu.style.display = 'none';
         }
     },
     bindUiEvents: function () {
         var _uiEvents = demoTools.uiEvents;
-        $('.js-propertySwitcher').on('change', _uiEvents.justifyChange);
+        $('.js-propertySwitcher').on('change', _uiEvents.inputChange);
         $('#js-cloneItem').on('click', _uiEvents.cloneItem);
+        $('.promo__figure').on('contextmenu', _uiEvents.figureContextMenu);
+        $('.tutor__field--contextmenu').on('mouseleave', _uiEvents.figureContextMenuMouseout);
+
     },
     activatePlugins: function () {
-
+        $('.promo__figure').editable({stylable: true})
     },
     functions: {}
 };
